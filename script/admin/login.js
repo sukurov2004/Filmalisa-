af4e2d2b59e22333f8d7dc55a1e0fd727697c790
sukurov2004 <maliksukurov27@gmail.com>
2026-02-23 00:48:26 +0400

    Implement admin login/logout functionality and secure dashboard access

---
+++ script/admin/login.js
@@ -0,0 +1,35 @@
+document.querySelector('.form').addEventListener('submit', async function (e) {
+  e.preventDefault();
+
+  const username = document.querySelector('.loginName').value.trim();
+  const password = document.querySelector('.loginPassword').value.trim();
+
+  try {
+    const response = await fetch('https://api.sarkhanrahimli.dev/api/filmalisa/auth/admin/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: username,
+        password: password,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok || !data.result) {
+      alert(data.message || 'Login failed!');
+      return;
+    }
+    localStorage.setItem('adminToken', data.data.tokens.access_token);
+    window.location.href = 'http://127.0.0.1:5500/pages/admin/dashboard.html';
+  } catch (error) {
+    console.error('Error:', error);
+    alert('Something went wrong. Please try again.');
+  }
+});
+document.querySelector('.eye').addEventListener('click', function () {
+  const passwordInput = document.querySelector('.loginPassword');
+  passwordInput.type = passwordInput.type === 'password' ? 'text' : 'password';
+});
