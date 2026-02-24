ae7767bc48ee946e95e40d9c299c211f47b4ac18
sukurov2004 <maliksukurov27@gmail.com>
2026-02-24 23:04:56 +0400

    fix: update redirect URLs to local development server for admin scripts

--- script/admin/actors.js
+++ script/admin/actors.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const token = localStorage.getItem("adminToken");
   if (!token) {
-    window.location.href = "https://sukurov2004.github.io/Filmalisa-/index.html";
+    window.location.href = "http://127.0.0.1:5500/index.html";
   }
 });
 
