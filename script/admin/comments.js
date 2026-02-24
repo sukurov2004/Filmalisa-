2db9bc2da048e29a3a23e0605477958ed34cca65
kasimovahokume <kasimovahokum@gmail.com>
2026-02-25 00:48:50 +0400

    comments api and images fixes

--- script/admin/comments.js
+++ script/admin/comments.js
@@ -24,7 +24,30 @@ async function getComments() {
 
     const data = await response.json();
     
-    console.log("Gələn məlumat:", data);
+    // console.log("Gələn məlumat:", data);
+
+    if (data.data && Array.isArray(data.data)){
+      commentsBody.innerHTML = "";
+      data.data.forEach((item,index) => {
+        const tr = document.createElement("tr");
+
+        tr.innerHTML = `
+           <td>${index + 1}</td>
+                <td>
+                    <img src="${item.movie?.cover_url || '../../assets/Admin/images/movies.svg'}" 
+                         style="width: 45px; height: 60px; object-fit: cover; border-radius: 4px;"
+                         onerror="this.src='../../assets/Admin/images/movies.svg'">
+                </td>
+                <td>${item.movie?.title || "Film adı yoxdur"}</td>
+                <td>${item.comment}</td> 
+                <td>
+                    <i class="fa-solid fa-trash delete-icon" data-id="${item.id}" style="cursor:pointer; color:red; background: #ef444426;"></i>
+                </td>
+        `;
+
+        commentsBody.appendChild(tr);
+      });
+    }
 
   } catch (error) {
     console.log("Xəta baş verdi:", error);
