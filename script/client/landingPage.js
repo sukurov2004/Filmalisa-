95cae5285c3f7266bc5edb733f007a8b9c9e6634
sukurov2004 <maliksukurov27@gmail.com>
2026-02-28 00:54:43 +0400

    created landingPage profil and logout buttons

--- script/client/landingPage.js
+++ script/client/landingPage.js
@@ -1,8 +1,32 @@
-// ===== AUTH GUARD =====
-(function () {
+document.addEventListener("DOMContentLoaded", () => {
   const token = localStorage.getItem("token");
 
+  const signInBtn = document.querySelector(".sign-in-btn");
+  const userMenu = document.querySelector(".user-menu");
+  const userIcon = document.querySelector(".user-icon");
+  const dropdown = document.querySelector(".dropdown");
+  const logoutBtn = document.querySelector(".logout-btn");
+
   if (token) {
-    window.location.replace("http://127.0.0.1:5500/pages/client/home.html");
+    signInBtn.classList.add("hidden");
+    userMenu.classList.remove("hidden");
   }
-})();
+
+  // Dropdown toggle
+  userIcon.addEventListener("click", () => {
+    dropdown.classList.toggle("hidden");
+  });
+
+  // Logout
+  logoutBtn.addEventListener("click", () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  });
+
+  // Click outside close
+  document.addEventListener("click", (e) => {
+    if (!userMenu.contains(e.target)) {
+      dropdown.classList.add("hidden");
+    }
+  });
+});
